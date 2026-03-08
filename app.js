9ed2184a845060a896479e7232602f94a0217c82
kseniach123-stack <kseniach123@gmail.com>
2026-03-08 15:34:43 +0000

    Fix formatting issue in LumeTerminal object

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-﻿ const LumeTerminal = {state: {
+ const LumeTerminal = {state: {
         inventory: [
             { id: 1, name: "Eau de toilette", price: 160.00, img: "https://images.unsplash.com/photo-1606334585230-3ba76447cdbd?q=80&w=863&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
             { id: 2, name: "Cream Set", price: 115.00, img: "https://images.unsplash.com/photo-1647492989217-afaf693e19b8?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
